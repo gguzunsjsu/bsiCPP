f07f428221761a6333d4ed798620a041bc49be18
gguzunsjsu <gheorghi.guzun@sjsu.edu>
2022-08-08 16:18:13 -0700

    Added examples

---
+++ examples/example.cpp
@@ -0,0 +1,22 @@
+//
+// Created by gheor on 5/14/2022.
+//
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <chrono>
+#include <random>
+
+#include "BsiUnsigned.hpp"
+#include "BsiSigned.hpp"
+#include "BsiAttribute.hpp"
+#include "hybridBitmap/hybridbitmap.h"
+#include "hybridBitmap/UnitTestsOfHybridBitmap.hpp"
+#include "NNUtilities.hpp"
+
+int main(){
+    cout<<"Hello"<<endl;
+}
