8c6ebc5fa46404b224524b85956cceefbcfb7f51
Lypho2012 <ioi2020acsl@gmail.com>
2024-04-11 10:27:54 -0700

    update testing inverse to test cmake build

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <chrono>
 
-#include "BsiUnsigned.hpp"
-#include "BsiSigned.hpp"
-#include "BsiAttribute.hpp"
+#include </home/czhang/OpenSfM/bsiCPP/bsi/BsiUnsigned.hpp>
+#include </home/czhang/OpenSfM/bsiCPP/bsi/BsiSigned.hpp>
+#include </home/czhang/OpenSfM/bsiCPP/bsi/BsiAttribute.hpp>
 
 
 //void MultiplyVectorByScalar(vector<long>& v, int k) {
@@ -12,8 +12,15 @@
 using namespace std;
 vector<BsiAttribute<uint64_t>*> inv(vector<BsiAttribute<uint64_t>*> matrix);
 void sgesv(int n, int m, vector<BsiAttribute<uint64_t>*> a, vector<int> ipiv, vector<BsiAttribute<uint64_t>*> b);
-void main() {
-
+int main() {
+    BsiSigned<uint64_t> bsi;
+    vector<BsiAttribute<uint64_t>*> mat;
+    vector<long> r1 = {4,3};
+    vector<long> r2 = {3,2};
+    mat.push_back(bsi.buildBsiAttributeFromVectorSigned(r1, 0.5));
+    mat.push_back(bsi.buildBsiAttributeFromVectorSigned(r2, 0.5));
+    vector<BsiAttribute<uint64_t>*> res = inv(mat);
+    return 0;
 }
 
 vector<BsiAttribute<uint64_t>*> inv(vector<BsiAttribute<uint64_t>*> mat) {
@@ -21,7 +28,7 @@ vector<BsiAttribute<uint64_t>*> inv(vector<BsiAttribute<uint64_t>*> mat) {
     vector<int> ipiv;
     vector<BsiAttribute<uint64_t>*> res;
     for (int i=0; i<n; i++) {
-        res.push_back(new BsiSigned<uint64_t>(mat[0].size));
+        res.push_back(new BsiSigned<uint64_t>(mat.at(0)->getNumberOfSlices()+1));
     }
     sgesv(n,n,mat,ipiv,res);
 }
