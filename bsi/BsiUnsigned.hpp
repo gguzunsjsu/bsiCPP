a8ae6fa3875e367a8edc5f2e650948088719a702
ParthPanchal <pcpanchal284@gmail.com>
2025-08-29 10:45:03 -0700

    Modified negate for twoscomplement(with decimal).

--- bsi/BsiUnsigned.hpp
+++ bsi/BsiUnsigned.hpp
@@ -2719,14 +2719,52 @@ void BsiUnsigned<uword>::reset(){
 
 };
 
+// template <class uword>
+// BsiVector<uword>* BsiUnsigned<uword>::negate(){
+//     BsiVector<uword>* res = new BsiUnsigned<uword>();
+//     res->bsi = this->bsi;
+//     res->sign = new HybridBitmap<uword>(this->getNumberOfRows(),true);
+//     res->is_signed = true;
+//     res->twosComplement = false;
+//     res->setNumberOfRows(this->getNumberOfRows());
+//     return res;
+// };
+
 template <class uword>
-BsiVector<uword>* BsiUnsigned<uword>::negate(){
-    BsiVector<uword>* res = new BsiUnsigned<uword>();
-    res->bsi = this->bsi;
-    res->sign = new HybridBitmap<uword>(this->getNumberOfRows(),true);
-    res->is_signed = true;
-    res->twosComplement = false;
+BsiVector<uword>* BsiUnsigned<uword>::negate() {
+    HybridBitmap<uword> onesBitmap;
+
+    onesBitmap.setSizeInBits(this->bsi[0].sizeInBits(), true);
+    onesBitmap.setSizeInBits(this->bsi[0].sizeInBits());
+
+    onesBitmap.density = 1;
+
+    int signSliceSize = this->firstSlice ? 2 : 1;
+
+    BsiSigned<uword>* res = new BsiSigned<uword>(this->getNumberOfSlices() + signSliceSize);
+
+    // Step 4: Invert all bit slices
+    for (int i = 0; i < this->getNumberOfSlices(); ++i) {
+        res->bsi.push_back(this->bsi[i].Not());  // Bitwise NOT
+        res->numSlices++;
+    }
+
+    res->addSlice(onesBitmap);
+
+    if (this->firstSlice) {
+        res->addOneSliceNoSignExt(onesBitmap);
+    }
+
+    res->existenceBitmap = this->existenceBitmap;
+    res->setPartitionID(this->getPartitionID());
+    res->sign = res->bsi[res->numSlices - 1];
     res->setNumberOfRows(this->getNumberOfRows());
+    if (res->sign.density != 0) res->is_signed = true;
+    else res->is_signed = false;
+    res->firstSlice = this->firstSlice;
+    res->lastSlice = this->lastSlice;
+    res->setTwosFlag(true);
+    res->decimals = this->decimals;
     return res;
 };
 
